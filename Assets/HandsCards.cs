6189589b1ba52039a4b637ea6292e85863facfe5
=nikita.dovgal0308@mail.ru <nikita.dovgal0308@mail.ru>
2023-09-24 14:51:30 +0300

    ER

--- Assets/HandsCards.cs
+++ Assets/HandsCards.cs
@@ -8,7 +8,7 @@ public class HandsCards : MonoBehaviour
     [SerializeField] private Mana mana; 
     [SerializeField] private int maxHand = 5;
     [SerializeField] private List<Card> deka = new List<Card>();
-    [SerializeField] private GameObject[] dekaCards;
+    [SerializeField] private CardHandView[] dekaCards;
     [SerializeField] private Transform[] dekaCardsPositionsActivate;
     [SerializeField] private Transform[] dekaCardsPositionsIdle;
     [SerializeField] private bool[] dekaCardsIsActive;
